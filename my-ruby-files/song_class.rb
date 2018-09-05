d9d0dbe725f2b2dc1495b2eb2e43e75162034409
Steven Crouse <sdcrouse91@gmail.com>
2018-09-05 02:18:54 +0000

    Updated song_class.rb

--- my-ruby-files/song_class.rb
+++ my-ruby-files/song_class.rb
@@ -1,15 +1,20 @@
-class Song
- 
+ class Song
+  puts "This only gets executed once!"
+  
   @@all = []
  
   attr_accessor :name
  
   def initialize(name)
     @name = name
-    @@all << self
+    # @@all << self
+    self.class.all << self # This works despite the fact that #self.all is defined BELOW.
   end
  
-  def self.all
+  def self.all # This won't do anything until it is actually called.
+  # It will NOT get executed when the Song class is first defined.
+  # However, it WILL get defined with the rest of the Song class before a new Song is made.
+  # That is why it is OK to define this method AFTER #initialize.
     @@all
   end
   
@@ -18,7 +23,13 @@ class Song
   end
 end
 
+puts "What is the order of execution here?"
+# Answer: the #puts statement at the top of the Song class,
+# then the #puts statement above this comment, then the code below.
+# The #puts statement in the Song class only gets executed the first time that the Song class is defined!
+puts "This gets executed next, and then the thriller Song is defined."
 thriller = Song.new("Thriller")
+puts "This gets executed after that!"
 yellow_submarine = Song.new("Yellow Submarine")
 god_of_wonders = Song.new("God of Wonders")
 jingle_bells = Song.new("Jingle Bells")
